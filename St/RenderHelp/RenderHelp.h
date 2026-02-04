9ddce53cd6100cfe758a8f43942ff67d682b8d03
NiurouLee <112259092+NiurouLee@users.noreply.github.com>
2026-02-04 23:28:09 +0800

    render .h

--- St/RenderHelp/RenderHelp.h
+++ St/RenderHelp/RenderHelp.h
@@ -2,6 +2,7 @@
 #define _RENDER_HELP_H_
 
 #include <assert.h>
+#include <cstdint>
 #include <math.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -897,6 +898,132 @@ typedef Matrix<3,4,float> Mat3x4f;
 //-----------------
 //3D数学运算
 //-----------------
+
+//矢量转整数运算
+inline  static uint32_t vector_to_color(const Vec4f &color)
+{
+  uint32_t r=(uint32_t)Between(0,255,(int)(color.r*255.0f));
+  uint32_t g=(uint32_t)Between(0,255,(int)(color.g*255.0f));
+  uint32_t b=(uint32_t)Between(0,255,(int)(color.b*255.0f));
+  uint32_t a=(uint32_t)Between(0,255,(int)(color.a*255.0f));
+  return (r<<16)|(g<<8)|b|(a<<24);
+}
+
+
+//矢量转整数颜色
+inline static uint32_t vector_to_color(const Vec3f &color)
+{
+  return vector_to_color(color.xyz1());
+}
+
+//整数颜色到矢量
+inline static Vec4f vector_form_color(uint32_t rgba)
+{
+  Vec4f out;
+  out.r= ((rgba>>16)&0xff)/255.0f;
+  out.g= ((rgba>>8)&0xff)/255.0f;
+  out.b= ((rgba>>0)&0xff)/255.0f;
+  out.a=((rgba>>24)&0xff)/255.0f;
+  return out;
+}
+
+//matrix set to zero
+inline static Mat4x4f matrix_set_zero()
+{
+  Mat4x4f m;
+  m.m[0][0]=m.m[0][1]=m.m[0][2]=m.m[0][3]=0.0f;
+  m.m[1][0]=m.m[1][1]=m.m[1][2]=m.m[1][3]=0.0f;
+  m.m[2][0]=m.m[2][1]=m.m[2][2]=m.m[2][3]=0.0f;
+  m.m[3][0]=m.m[3][1]=m.m[3][2]=m.m[3][3]=0.0f;
+  return m;
+}
+
+//set to identity
+inline static Mat4x4f matrix_set_identity()
+{
+  Mat4x4f m;
+  m.m[0][0]=m.m[1][1]=m.m[2][2]=m.m[3][3]=1.0f;
+  m.m[0][1]=m.m[0][2]=m.m[0][3]=0.0f;
+  m.m[1][0]=m.m[1][2]=m.m[1][3]=0.0f;
+  m.m[2][0]=m.m[2][1]=m.m[2][3]=0.0f;
+  m.m[3][0]=m.m[3][1]=m.m[3][2]=0.0f;
+  return m;
+}
+
+
+//平移变换
+inline static Mat4x4f matrix_set_translate(float x,float y,float z)
+{
+  Mat4x4f m=matrix_set_identity();
+  m.m[3][0]=x;
+  m.m[3][1]=y;
+  m.m[3][2]=z;
+  return m;
+}
+
+//缩放变换
+inline static Mat4x4f matrix_set_scale(float x, float y, float z)
+{
+  Mat4x4f m= matrix_set_identity();
+  m.m[0][0]=x;
+  m.m[1][1]=y;
+  m.m[2][2]=z;
+  return m;
+}
+
+//旋转变换 围绕(x,y,z)矢量旋转 theta 角度
+inline static Mat4x4f matrix_set_rotate(float x,float y,float z,float theta)
+{
+  float gsin= (float)sin(theta*0.5f);
+  float gcos= (float)cos(theta*0.5f);
+  float w= gcos;
+  Vec3f vec=vector_normalize(Vec3f(x,y,z));
+  x=vec.x*gsin;
+  y=vec.y*gsin;
+  z=vec.z*gsin;
+  Mat4x4f m;
+  m.m[0][0]=1-2*y*y-2*z*z;
+  m.m[1][0]=2*x*y-2*w*z;
+  m.m[2][0]=2*x*z+2*w*y;
+  m.m[0][1]=2*x*y+2*w*z;
+  m.m[1][1]=1-2*x*x-2*z*z;
+  m.m[2][1]=2*y*z-2*w*x;
+  m.m[0][2]=2*x*z-2*w*y;
+  m.m[1][2]=2*y*z+2*w*x;
+  m.m[2][2]=1-2*x*x-2*y*y;
+  m.m[0][3]=m.m[1][3]=m.m[2][3]=0.0f;
+  m.m[3][0]=m.m[3][1]=m.m[3][2]=0.0f;
+  m.m[3][3]=1.0f;
+  return m;
+}
+
+//摄像机变换矩阵 eye/视点为止，at/看向哪里，up/指向上方的矢量
+inline static Mat4x4f matrix_set_lookat(const Vec3f &eye,const Vec3f &at, const Vec3f &up)
+{
+  Vec3f zaxis=vector_normalize(at-eye);
+  Vec3f xaxis=vector_normalize(vector_cross(up,zaxis));
+  Vec3f yaxis= vector_cross(zaxis,xaxis);
+  Mat4x4f m;
+  m.SetCol(0,Vec4f(xaxis.x,xaxis.y,xaxis.z,-vector_dot(eye,xaxis)));
+  m.SetCol(1,Vec4f(yaxis.x,yaxis.y,yaxis.z,-vector_dot(eye,yaxis)));
+  m.SetCol(3,Vec4f(0.0f,0.0f,0.0f,1.0f));
+  return m;
+}
+
+//D3DXMatrixPerspectiveFovLH
+inline static Mat4x4f matrix_set_perspective(float fovy,float aspect ,float zn,float zf)
+{
+  float fax= 1.0f/(float)tan(fovy*0.5f);
+  Mat4x4f m= matrix_set_zero();
+  m.m[0][0]=(float)(fax/aspect);
+  m.m[1][1]=(float)(fax);
+  m.m[2][2]=zf/(zf-zn);
+  m.m[3][2]=-zn*zf/(zf-zn);
+  m.m[2][3]=1;
+  return m;
+}
+
+
 
 
 
